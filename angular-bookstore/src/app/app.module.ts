f8a77b9f226fc63252a3e1a6ab722bdc80bd0d54
Stevensjsoseph95 <stevensjoseph95@gmail.com>
2020-04-13 15:25:02 -0500

    First time going to merge.

--- angular-bookstore/src/app/app.module.ts
+++ angular-bookstore/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import {PathNotFoundComponent } from './components/path-not-found/path-not-found.component'
 
 import { AppComponent } from './app.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookService } from './services/book.service';
 
+const routes: Routes=[
+{path: 'books',component: BookListComponent},
+{path: 'category/:id',component: BookListComponent},
+{path: '',redirectTo:'/books',pathMatch:'full'},
+{path: '**',component: PathNotFoundComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    BookListComponent,
+    PathNotFoundComponent
   ],
   imports: [
     BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      RouterModule.forRoot(routes)
   ],
   providers: [
     BookService
--- onlinebookstore/src/main/java/is/joseph/onlinebookstore/OnlinebookstoreApplication.java
+++ onlinebookstore/src/main/java/is/joseph/onlinebookstore/OnlinebookstoreApplication.java
@@ -9,5 +9,5 @@ public class OnlinebookstoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OnlinebookstoreApplication.class, args);
 	}
-
+	
 }
